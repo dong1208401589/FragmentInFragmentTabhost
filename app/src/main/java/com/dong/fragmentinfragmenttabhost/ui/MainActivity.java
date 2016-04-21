c72699ca376c870cc892bbb801f95250ec8d99cd
dong <dong1208401589@163.com>
2016-04-21 15:52:14 +0800

    空项目

---
+++ app/src/main/java/com/dong/fragmentinfragmenttabhost/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dong.fragmentinfragmenttabhost.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dong.fragmentinfragmenttabhost.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
